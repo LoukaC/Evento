4cca5f0fb104228fe002bbdbda4c03362fbc7869
Louka <louka.cipolat@yahoo.fr>
2024-03-19 19:12:50 +0000

    events page header logo

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import EventsList from '@/components/events-list'
+import H1 from '@/components/h1'
+import { EventoEvent } from '@/lib/types'
 
-export default function EventsPage() {
+type EventsPageProps = {
+  params: {
+    city: string
+  }
+}
+
+
+export default async function EventsPage({params}: EventsPageProps) {
+  
+  const city = params.city
+  
+  // fetch directly in a server component
+  const rep = await fetch(`https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`)
+  const events : EventoEvent[] = await rep.json()
+  
+
   return (
-    <div>
-      Events Page
-    </div>
+    <main className='flex flex-col items-center py-24 px-[20px] min-h-[100vh]'>
+      <H1 className="mb-28" >
+        {
+          city === 'all' ? "All Events" : "Events in " + city.charAt(0).toUpperCase() + city.slice(1) 
+        }
+      </H1>
+      <EventsList events={events}/>
+    </main>
   )
 }
